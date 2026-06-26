3093f5719d6286e5134526a36303a9a7c4958709
khomzy <ngowelak@gmail.com>
2026-06-26 22:32:03 +0200

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,13 @@
-window.SUPABASE_URL = 'https://dtcuhmovbkbrqhqkfssw.supabase.co';
-window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0Y3VobW92YmticnFocWtmc3N3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwODA0NTIsImV4cCI6MjA5NjY1NjQ1Mn0.RyIIjTqJFYtSFCPQrJS2sq9uQgaCvXLR0mnU6fysXx0';
+(function () {
+    const projectUrl = 'https://inorxdoginyirqlptbgo.supabase.co';
+    const anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlub3J4ZG9naW55aXJxbHB0YmdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE0NjMwNDksImV4cCI6MjA5NzAzOTA0OX0.a_zedSMJXFmmVsituRU-x4BBuEqByY2N5hLirvApnK0';
+
+    window.SUPABASE_PROJECT_URL = projectUrl;
+    window.SUPABASE_ANON_KEY = anonKey;
+
+    if (window.supabase && typeof window.supabase.createClient === 'function') {
+        window.supabaseClient = window.supabase.createClient(projectUrl, anonKey);
+    } else {
+        console.error('Supabase library was not loaded before supabase-config.js');
+    }
+})();
